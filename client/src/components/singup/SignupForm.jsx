68049b51323702d6bf41d949366f9dc8c5133491
{hc456123@gmail.com} <hc456123@gmail.com>
2022-02-27 21:21:56 +0900

    Add Valid Id Check
    아이디 중복 체크

--- client/src/components/singup/SignupForm.jsx
+++ client/src/components/singup/SignupForm.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import {ColorBtn} from '../../styles/common/buttons';
 import {ValidCheck} from '../../styles/common/validtext';
-import { useRef } from 'react';
-import { signUp } from '../../network/signup/http';
+import { useState, useRef } from 'react';
+import { checkId, signUp } from '../../network/signup/http';
 
 const SignupFormBox = styled.form`
   width: 60%;
@@ -56,28 +56,57 @@ const SignupValid = styled(ValidCheck)`
   font-size: 11px;
 `;
 
+const ValidId = styled(SignupValid)`
+  color: ${({current}) => current ? "green" : "red"};
+`;
+
 function SignupForm () {
+  const [message, setMessage] = useState({
+    name: "",
+    email: "",
+    nickName: "",
+    password: "",
+  });
+  const [valid, setValid] = useState({
+    name: 0,
+    email: 0,
+    nickName: 0,
+    password: 0,
+  });
+
   const userId = useRef(null);
   const userEmail = useRef(null);
   const userName = useRef(null);
   const userBirthday = useRef(null);
   const userPw = useRef(null);
+  const userPw2 = useRef(null);
 
-  const handleSignupClick = () => {
+  const handleBlurId = (evt) => {
+    checkId(evt.target.value)
+      .then(({data: {success, message}}) => {
+        setMessage({...message, name: message});
+        setValid({...valid, name: evt.target.value === "" ? 0 : success });
+      });
+  }
+
+  const handleSignupClick = (evt) => {
+    evt.preventDefault();
+
     let body = {
-      user_id: userId.current.value,
-      user_email: userEmail.current.value,
-      user_name: userName.current.value,
-      user_birthday: userBirthday.current.value,
-      user_password: userPw.current.value,
+      name: userId.current.value,
+      email: userEmail.current.value,
+      nickname: userName.current.value,
+      birthday: userBirthday.current.value,
+      password: userPw.current.value,
+      password2: userPw2.current.value,
     }
-    signUp(body);
+    signUp(body).then((res) => console.log(res.data));
   };
 
   return (
     <SignupFormBox method='POST'>
-      <Input type="text" placeholder='아이디' ref={userId} />
-      <SignupValid> 이미 사용중이거나 탈퇴한 아이디입니다. </SignupValid>
+      <Input type="text" onBlur={handleBlurId} placeholder='아이디' ref={userId} />
+      {valid.name !== 0 && <ValidId current={valid.name}> {message.name} </ValidId>}
       <Input type="email" placeholder='이메일' ref={userEmail} />
       <SignupValid> 이미 사용중이거나 탈퇴한 이메일입니다. </SignupValid>
       <Input type="text" placeholder='닉네임' ref={userName} />
@@ -85,11 +114,11 @@ function SignupForm () {
       <Input type="text" placeholder='생년월일' ref={userBirthday} required/>
       <Input type="password" placeholder="비밀번호" ref={userPw} />
       <SignupValid> 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요. </SignupValid>
-      <Input type="password" placeholder='비밀번호 확인' />
+      <Input type="password" placeholder='비밀번호 확인' ref={userPw2} />
       <SignupValid> 비밀번호가 일치하지 않습니다. </SignupValid>
       <BtnBox>
         <CancelBtn>취소</CancelBtn>
-        <SignupBtn type="button" onClick={handleSignupClick}>회원가입</SignupBtn>
+        <SignupBtn onClick={handleSignupClick}>회원가입</SignupBtn>
       </BtnBox>
     </SignupFormBox>
   );
