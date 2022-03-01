1c66a3412eda75a69ba2b4e828947810086b2b62
Park Na Yeon <within197@naver.com>
2022-03-01 17:19:39 +0900

    [FEAT] Add post CRUD Route and Controller

--- server/models/Post.js
+++ server/models/Post.js
@@ -5,13 +5,15 @@ const postSchema = new mongoose.Schema({
   title: String,
   textContent: String,
   targetAge: Number,
-  tagData: [{ type: String }],
+  tagArray: [{ type: String }],
   meta: {
     views: { type: Number, default: 0, required: true },
   },
   // comments: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Comment' }],
   // writer_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   createAt: { type: Date, required: true, default: Date.now },
+  updateAt: { type: Date },
+  being: { type: Boolean, default: true }, // 게시글의 삭제 유무
 });
 
 const Post = mongoose.model('Post', postSchema);
