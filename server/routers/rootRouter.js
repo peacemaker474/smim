3c5aae2e6cbccdb112a505063c9767d14c1ebeea
Park Na Yeon <within197@naver.com>
2022-03-08 17:26:07 +0900

    [FEAT] Add google oauth controller

--- server/routers/rootRouter.js
+++ server/routers/rootRouter.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import { getCheckEmail, getCheckId, getCheckName, postSignup } from "../controllers/userController.js";
+import {
+  getCheckEmail,
+  getCheckId,
+  getCheckName,
+  postSignup,
+  getGoogleAuth,
+  getGoogleCallback,
+} from '../controllers/userController.js';
 
 const rootRouter = express.Router();
 
+rootRouter.get('/signup/id-check', getCheckId);
+rootRouter.get('/signup/email-check', getCheckEmail);
+rootRouter.get('/signup/name-check', getCheckName);
+rootRouter.post('/signup', postSignup);
 
-rootRouter.get("/signup/id-check", getCheckId);
-rootRouter.get("/signup/email-check", getCheckEmail);
-rootRouter.get("/signup/name-check", getCheckName);
-rootRouter.post("/signup", postSignup);
+// google oauth
+rootRouter.get('/login/google', getGoogleAuth);
+rootRouter.get('/login/google/callback', getGoogleCallback);
 
 export default rootRouter;
