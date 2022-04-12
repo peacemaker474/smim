82442a3ea5a1707269afdb0663c43b5893620c0f
Park Na Yeon <within197@naver.com>
2022-04-12 17:06:43 +0900

    [CHORE] Update url, module

--- client/src/network/post/http.js
+++ client/src/network/post/http.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 const http = 'http://localhost:4000';
 
 export const postUpload = (data, header) => {
-  return axios.post(`${http}/posts/create`, data, header);
+  return axios.post(`${http}/post/create`, data, header);
 };
