30f0436d834df93183c4cf0fd74c5dc1b3d19bf8
Park Na Yeon <within197@naver.com>
2022-04-25 19:27:24 +0900

    [FEAT] Update controller for like,view,upload, del

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -2,11 +2,40 @@ import Post from '../models/Post.js';
 import User from '../models/User.js';
 // import Tag from '../models/Tag.js';
 
-// 게시물 생성
+// 게시물 생성(Post Create)
 export const postCreate = async (req, res) => {
   const { title, content, tag, targetAge } = req.body;
   console.log(req.body);
-  try {
+
+  if (!title) {
+    return res.json({
+      success: false,
+      message: 'title이 undefined입니다.',
+    });
+  } else if (!tag) {
+    return res.json({
+      success: false,
+      message: 'tag가 undefined입니다.',
+    });
+  } else if (!content) {
+    return res.json({
+      success: false,
+      message: 'content가 undefined입니다.',
+    });
+  } else if (
+    !(
+      targetAge === '10' ||
+      targetAge === '20' ||
+      targetAge === '30' ||
+      targetAge === '40' ||
+      targetAge === '50'
+    )
+  ) {
+    return res.json({
+      success: false,
+      message: '해당 연령대는 존재하지 않습니다',
+    });
+  } else {
     await Post.create({
       title,
       tagArray: tag,
@@ -14,17 +43,12 @@ export const postCreate = async (req, res) => {
       targetAge,
       owner: req.body.user.user_id,
     });
-
     return res.json({
       success: true,
       message: '새로운 게시글 작성이 완료되었습니다.',
     });
-  } catch (error) {
-    console.log(error);
-    return res.json({ Error: error });
   }
 };
-
 // 게시물 수정
 export const putEdit = async (req, res) => {
   const { id } = req.params;
@@ -37,44 +61,65 @@ export const putEdit = async (req, res) => {
   return res.json(id);
 };
 
-// 게시물 보기
+// 게시물 보기(Post Detail Read)
 export const getPostDetail = async (req, res) => {
   const { id } = req.params;
-  const exist = await Post.exists({ _id: id });
-  if (exist) {
-    const postData = await Post.findById(id);
-    return res.json(postData);
+  const exist = await Post.exists({ _id: id, being: true });
+  if (!exist) {
+    return res.json({
+      success: false,
+      message: '존재하지 않거나 삭제된 게시물입니다.',
+    });
   }
-  return res.json({ result: 'success' });
+
+  const postData = await Post.findById(id);
+  return res.json(postData);
 };
 
-// 나이별 게시물 보기
+// 나이별 게시물 보기(Post List Read)
 export const getPostList = async (req, res) => {
   const { age } = req.query;
+
+  if (!(age === '10' || age === '20' || age === '30' || age === '40' || age === '50')) {
+    return res.json({
+      success: false,
+      message: '해당 연령대는 존재하지 않습니다',
+    });
+  }
   const postList = await Post.find({ age });
-  console.log(postList);
   res.json(postList);
 };
 
-// 게시물 삭제
+// 게시물 삭제(Post List Delete)
 export const deletePost = async (req, res) => {
-  const { id } = req.params;
-  const postData = await Post.exists({ _id: id });
+  const { id } = req.params; // post id
+  const { user_id } = req.body.user; // user id
+  const postData = await Post.find({ _id: id, owner: user_id, being: true });
 
-  if (!postData) {
-    return res.status(400);
+  if (postData.length === 0) {
+    return res.json({
+      success: false,
+      message: '존재하지 않거나 삭제된 게시물입니다.',
+    });
   }
 
   await Post.findByIdAndUpdate(id, { being: false });
-  return res.json({ result: 'success' });
+  return res.json({
+    success: true,
+    message: '게시글 삭제가 완료되었습니다.',
+  });
 };
 
+// 게시물 조회수
 export const getPostView = async (req, res) => {
   const { id } = req.params;
+  const post = await Post.find({ _id: id, being: true });
 
-  const post = await Post.findById({ _id: id });
-  if (!post) {
-    return res.json('not found');
+  if (post.length === 0) {
+    return res.json({
+      success: false,
+      message: '존재하지 않거나 삭제된 게시물입니다.',
+    });
   }
 
   post.meta.views += 1;
@@ -82,38 +127,60 @@ export const getPostView = async (req, res) => {
   return res.json('succes');
 };
 
+// 게시물 좋아요
 export const getPostLike = async (req, res) => {
   const { id } = req.params;
   const { user_id } = req.body.user;
-  const post = await Post.findById({ _id: id });
+  const post = await Post.findOne({ _id: id, being: true });
   const user = await User.findById({ _id: user_id });
-  console.log(user);
-  if (!post) {
-    return res.json('not found');
+
+  if (post.length === 0) {
+    return res.json({
+      success: false,
+      message: '존재하지 않거나 삭제된 게시물입니다.',
+    });
   }
+
   post.meta.likes += 1;
   await post.save();
 
   user.likes.push(id);
   await user.save();
 
-  return res.json('succes');
+  return res.json({
+    success: true,
+    message: '좋아요를 눌렀습니다.',
+  });
 };
 
-// export const tagUpload = async (req, res) => {
-//   const { text } = req.body;
-//   try {
-//     await Tag.create({
-//       text,
-//     });
+// 게시물 좋아요 취소
+export const getPostUnlike = async (req, res) => {
+  const { id } = req.params;
+  const { user_id } = req.body.user;
+  const post = await Post.findOne({ _id: id, being: true });
+  const user = await User.findById({ _id: user_id });
 
-//     return res.json({
-//       success: true,
-//       post_id: '1111',
-//       message: '새로운 태그 작성이 완료되었습니다.',
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.json({ Error: error });
-//   }
-// };
+  if (post.length === 0) {
+    return res.json({
+      success: false,
+      message: '존재하지 않거나 삭제된 게시물입니다.',
+    });
+  }
+
+  if (!user.likes.includes(id)) {
+    return res.json({
+      success: true,
+      message: '좋아요를 누르지 않은 게시물입니다.',
+    });
+  }
+
+  post.meta.likes -= 1;
+  await post.save();
+  user.likes.push(id);
+  await user.save();
+
+  return res.json({
+    success: true,
+    message: '좋아요를 취소했습니다.',
+  });
+};
--- server/routers/postRouter.js
+++ server/routers/postRouter.js
@@ -7,6 +7,7 @@ import {
   getPostList,
   getPostView,
   getPostLike,
+  getPostUnlike,
 } from '../controllers/postController.js';
 import { verifyToken } from '../controllers/tokenControllers.js';
 
@@ -14,6 +15,11 @@ export const postRouter = express.Router();
 
 postRouter.get('/target', getPostList);
 postRouter.post('/create', verifyToken, postCreate);
-postRouter.route('/:id').get(getPostDetail).put(putEdit).delete(deletePost);
+postRouter
+  .route('/:id')
+  .get(getPostDetail)
+  .put(verifyToken, putEdit)
+  .delete(verifyToken, deletePost);
 postRouter.route('/:id/view').get(getPostView);
 postRouter.route('/:id/like').get(verifyToken, getPostLike);
+postRouter.route('/:id/unlike').get(verifyToken, getPostUnlike);
