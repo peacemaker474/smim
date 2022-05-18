7c4246b0cd0991df82ff89455eaf7cca92082cd6
Park Na Yeon <within197@naver.com>
2022-05-18 13:16:47 +0900

    [CHORE] Update like func in post page

--- client/src/components/PostView/Post/PostLike.jsx
+++ client/src/components/PostView/Post/PostLike.jsx
@@ -1,40 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import heartFill from '../../../asset/icon/icon-heart-fill.svg';
 import heartLine from '../../../asset/icon/icon-heart-line.svg';
 import { postLike, postUnlike } from '../../../network/post/http';
 import { getCookie } from '../../../utils/cookie';
 
-export default function PostLike({ like }) {
-  const [likeCheck, setLikeCheck] = useState(true);
+export default function PostLike({ quantity, like }) {
+  const [likeCheck, setLikeCheck] = useState(like);
   const location = useLocation();
   const tkn = getCookie('users');
   const id = location.pathname.split('view/')[1];
-  const [likeValue, setLikeValue] = useState(like);
-  const loginState = useSelector((state) => state.loginReducer);
-
-  console.log(loginState);
+  const [likeValue, setLikeValue] = useState(quantity);
 
   useEffect(() => {
-    setLikeValue(like);
-  }, [like]);
+    setLikeValue(quantity);
+    setLikeCheck(like);
+  }, [quantity, like]);
 
   const handleLikeClick = async () => {
     if (likeCheck) {
       // 좋아요 했을 때
       try {
-        const response = await postUnlike(id, {
+        await postUnlike(id, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${tkn}`,
           },
         });
-        console.log(response.data.success);
-        // setLikeCheck(false);
-        // setLikeValue((prev) => prev - 1);
-        // console.log(response.data);
+
+        setLikeCheck(false);
+        setLikeValue((prev) => prev - 1);
       } catch (error) {
         console.error(error);
       }
@@ -42,7 +38,7 @@ export default function PostLike({ like }) {
       // 좋아요 하지 않았을 때
 
       try {
-        const response = await postLike(id, {
+        await postLike(id, {
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${tkn}`,
@@ -50,7 +46,6 @@ export default function PostLike({ like }) {
         });
         setLikeCheck(true);
         setLikeValue((prev) => prev + 1);
-        console.log(response.data);
       } catch (error) {
         console.error(error);
       }
