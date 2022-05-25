b1a2f5e8bdc2c7722f1a59a8f39f1eafd400516e
Park Na Yeon <within197@naver.com>
2022-05-25 22:04:49 +0900

    [FEAT] Add reqeust comment function

---
+++ client/src/network/comment/http.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const http = 'http://localhost:4000';
+
+export const CommentListRead = (data) => {
+  return axios.get(`${http}/post/comment`, data);
+};
+
+export const CommentCreate = (data, header) => {
+  return axios.post(`${http}/post/comment`, data, header);
+};
