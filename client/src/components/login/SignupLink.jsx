01a0de4b012ddca7ff426feb0260d5eaf81dcfad
{hc456123@gmail.com} <hc456123@gmail.com>
2022-05-31 21:11:54 +0900

    [REFACTOR] Update Event Handler Naming
    로그인 이벤트 핸들러 네이밍 수정

--- client/src/components/login/SignupLink.jsx
+++ client/src/components/login/SignupLink.jsx
@@ -16,11 +16,11 @@ const SignLink = styled(Link)`
   font-weight: bold;
 `;
 
-function SignupLink ({handleLoginClose}) {
+function SignupLink ({onLoginClose}) {
   return (
     <SignBox>
       <SignText> 아직 회원이 아니신가요? </SignText>
-      <SignLink to="/signup" onClick={handleLoginClose}> 회원가입 </SignLink>
+      <SignLink to="/signup" onClick={onLoginClose}> 회원가입 </SignLink>
     </SignBox>
   )
 }
