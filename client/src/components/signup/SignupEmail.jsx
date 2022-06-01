0e48e368c5308cd077e41be5adf8c9cd19561c95
{hc456123@gmail.com} <hc456123@gmail.com>
2022-06-01 17:29:43 +0900

    [REFACTOR] Update Event Handler Naming
    회원가입 페이지 이벤트 핸들러 네이밍 수정

---
+++ client/src/components/signup/SignupEmail.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { checkEmail } from '../../network/signup/http';
+import { SignupInput } from '../../styles/common/input';
+import { ValidCheck } from '../../styles/common/validtext';
+import { InputBox } from '../../styles/signup/container';
+import { SignupTitle } from '../../styles/signup/title';
+import { emailValidation } from '../../utils/validation';
+
+function SignupEmail ({message, setMessage, valid, setValid, onInputChange}) {
+  const handleEmailBlur = (evt) => {
+    if (!emailValidation(evt.target.value)) {
+      setMessage({ ...message, email: '이메일 형식의 맞춰서 작성해주세요.'});
+      setValid({ ...valid, email: false});
+    } else {
+      checkEmail(evt.target.value)
+        .then(({data}) => {
+          setMessage({ ...message, email: data.message});
+          setValid({ ...valid, email: data.success});
+        });
+    }
+  }
+  return (
+    <InputBox>
+      <SignupTitle> 이메일 </SignupTitle>
+      <SignupInput type="email" name="email" onChange={onInputChange} onBlur={handleEmailBlur} />
+      {message.email !== "" && <ValidCheck current={valid.email}> {message.email} </ValidCheck> }
+    </InputBox>
+  )
+}
+
+export default SignupEmail;
