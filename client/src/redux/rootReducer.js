9cddc49f513540f3f1a536a84efe03f09ec41c69
Park Na Yeon <within197@naver.com>
2022-06-14 16:34:59 +0900

    [REFACTOR] Update rootReducer and store

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,25 +1,28 @@
-import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-
+import { combineReducers } from 'redux';
 import loginReducer from './login/reducer';
 import toggleReducer from './toggle/reducer';
-import postCreateReducer from './postCreate/reducer';
-import postFormReducer from './postForm/reducer';
-import commentReducer from './comment/reducer';
+// import postCreateReducer from './postCreate/reducer';
+import postCreateSlice from './postCreate/postCreateSlice';
+import postFormCheckSlice from './postForm/postFormCheckSlice';
+import commentSlice from './comment/commentSlice';
+// import postFormReducer from './postForm/reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whiteList: ['loginReducer'],
+  whiteList: ['login'],
 };
 
 const rootReducer = combineReducers({
-  loginReducer,
-  toggleReducer,
-  postCreateReducer,
-  postFormReducer,
-  commentReducer,
+  login: loginReducer,
+  toggle: toggleReducer,
+  postCreate: postCreateSlice,
+  postFormCheck: postFormCheckSlice,
+  comment: commentSlice,
 });
 
-export default persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
