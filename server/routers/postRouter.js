a79e6e7a88a8b59b69614450006a4e34604d5f6b
Park Na Yeon <within197@naver.com>
2022-06-19 22:45:39 +0900

    [REFACTOR] Add exist check conditional middleware

--- server/routers/postRouter.js
+++ server/routers/postRouter.js
@@ -9,28 +9,32 @@ import {
   getPostSearch,
 } from '../controllers/postController.js';
 import { getPostLike, getPostUnlike } from '../controllers/likeController.js';
-import { postBookmark, deleteBookmark, getBookmark } from '../controllers/bookmarkController.js';
+import { getBookmark, deleteBookmark } from '../controllers/bookmarkController.js';
 import { verifyToken } from '../controllers/tokenControllers.js';
 import { postCommentCreate, getCommentList } from '../controllers/commentController.js';
 import { fieldCheck } from '../middlewares/fieldCheck.js';
+import {
+  existPostCheckAndData,
+  existPostAndOwnerCheck,
+  existPostCheck,
+} from '../middlewares/existCheck.js';
 
 export const postRouter = express.Router();
 
 postRouter.get('/target', getPostList);
 postRouter.get('/search', getPostSearch);
 postRouter.post('/create', verifyToken, fieldCheck, postPostCreate);
-postRouter.get('/bookmark', verifyToken, getBookmark);
 postRouter.route('/comment').get(getCommentList).post(verifyToken, postCommentCreate);
 
 postRouter
   .route('/:id')
-  .get(getPostDetail)
-  .put(verifyToken, fieldCheck, putPostEdit)
-  .delete(verifyToken, deletePost);
+  .get(existPostCheckAndData, getPostDetail)
+  .put(verifyToken, fieldCheck, existPostAndOwnerCheck, putPostEdit)
+  .delete(verifyToken, existPostAndOwnerCheck, deletePost);
 
-postRouter.route('/:id/detail').get(verifyToken, getPostDetail);
-postRouter.route('/:id/view').get(getPostView);
-postRouter.route('/:id/bookmark').get(verifyToken, postBookmark);
-postRouter.route('/:id/unbookmark').get(verifyToken, deleteBookmark);
-postRouter.route('/:id/like').get(verifyToken, getPostLike);
-postRouter.route('/:id/unlike').get(verifyToken, getPostUnlike);
+postRouter.route('/:id/detail').get(verifyToken, existPostCheckAndData, getPostDetail);
+postRouter.route('/:id/view').get(existPostCheckAndData, getPostView);
+postRouter.route('/:id/bookmark').get(verifyToken, existPostCheck, getBookmark);
+postRouter.route('/:id/unbookmark').get(verifyToken, existPostCheck, deleteBookmark);
+postRouter.route('/:id/like').get(verifyToken, existPostCheckAndData, getPostLike);
+postRouter.route('/:id/unlike').get(verifyToken, existPostCheckAndData, getPostUnlike);
