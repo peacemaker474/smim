b3b301503bb1854f53d89bfdfcdb3718ff6091ce
Park Na Yeon <within197@naver.com>
2022-06-24 09:00:20 +0900

    [CHORE] Update mongo DB

--- server/models/Post.js
+++ server/models/Post.js
@@ -9,7 +9,7 @@ const postSchema = new mongoose.Schema({
   meta: {
     views: { type: Number, default: 0, required: true },
     likes: { type: Number, default: 0, required: true },
-    pinnedCmnt: { type: String, default: null, required: true },
+    pinnedCmnt: { type: String, required: false },
   },
   // comments: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Comment' }],
   createAt: { type: Date, required: true, default: Date.now },
--- server/models/User.js
+++ server/models/User.js
@@ -11,7 +11,6 @@ const userSchema = new mongoose.Schema({
   birthday: { type: Number, required: true },
   state: { type: Boolean, default: true, require: true }, // 탈퇴유무
   posts: [{ type: String }], // 작성한 게시글
-  likes: [{ type: String }], // 좋아요한 게시글
   bookmarks: [{ type: String }], // 즐겨찾기한 게시글
   socialOnly: { type: Boolean, default: false },
 });
