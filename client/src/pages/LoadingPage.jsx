40351f91311987fdabdd257f83424ccf959e16ab
{hc456123@gmail.com} <hc456123@gmail.com>
2022-06-27 09:54:56 +0900

    [FEAT] Add Loading Page
    로딩 페이지 추가

--- client/src/pages/LoadingPage.jsx
+++ client/src/pages/LoadingPage.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { HashLoader } from 'react-spinners';
+import styled from 'styled-components';
+
+const LoadingWrapper = styled.section`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 export default function LoadingPage() {
-  return <div></div>;
+  return (
+    <LoadingWrapper>
+      <HashLoader />
+    </LoadingWrapper>
+  );
 }
