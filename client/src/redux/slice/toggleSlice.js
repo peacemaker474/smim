b00ca99c175a72a227008cedd7e9437d3d304d3b
Park Na Yeon <within197@naver.com>
2022-06-28 20:22:06 +0900

    [FEAT] Add modal Toggle reducer

--- client/src/redux/slice/toggleSlice.js
+++ client/src/redux/slice/toggleSlice.js
@@ -1,9 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialToggleState = {
   menuToggled: false,
   loginToggled: false,
   inputToggled: false,
+  modalToggled: false,
 };
 
 const toggleSlice = createSlice({
@@ -18,12 +19,16 @@ const toggleSlice = createSlice({
     },
     inputToggle(state) {
       state.inputToggled = !state.inputToggled;
-    }
-  }
+    },
+    modalToggle(state) {
+      state.modalToggled = !state.modalToggled;
+    },
+  },
 });
 
 export const loginToggle = toggleSlice.actions.loginToggle;
 export const menuToggle = toggleSlice.actions.menuToggle;
 export const inputToggle = toggleSlice.actions.inputToggle;
+export const modalToggle = toggleSlice.actions.modalToggle;
 
 export default toggleSlice;
