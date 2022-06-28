7be345cf3d0d105ad00f30a66b2720bb354b5bfa
{hc456123@gmail.com} <hc456123@gmail.com>
2022-06-28 10:02:17 +0900

    [FEAT] Update Secure Key to Refresh Token
    리프레쉬 토큰 시크릿 코드 추가

--- server/controllers/tokenControllers.js
+++ server/controllers/tokenControllers.js
@@ -1,16 +1,17 @@
-import jwt, { decode } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-const SECRET_KEY = process.env.SECRET_KEY;
+const ACCESS_KEY = process.env.ACCESS_KEY;
+const REFRESH_KEY = process.env.REFRESH_KEY;
 
 export const createAccessToken = ( userId ) => {
-  return jwt.sign({ user_id: userId.toString() }, SECRET_KEY, {
+  return jwt.sign({ user_id: userId.toString() }, ACCESS_KEY, {
     expiresIn: '10m',
   });
 };
 
 export const createRefreshToken = ( userId ) => {
-  return jwt.sign({ user_id: userId.toString() }, SECRET_KEY, {
+  return jwt.sign({ user_id: userId.toString() }, REFRESH_KEY, {
     expiresIn: '30 days',
   });
 };
@@ -21,7 +22,7 @@ export const verifyToken = async (req, res, next) => {
   }
   const token = req.headers.authorization.split('Bearer ')[1];
 
-  jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+  jwt.verify(token, ACCESS_KEY, async (err, decoded) => {
     if (err) return res.status(500).json({ result: err });
     if (!decoded) {
       return res.status(500).json({ result: '유효하지 않은 토큰입니다.' });
@@ -38,13 +39,13 @@ export const verifyToken = async (req, res, next) => {
 export const reissueAccessToken = (req, res) => {
   const { refreshToken } = req.body;
 
-  jwt.verify(refreshToken, SECRET_KEY, async (err, decoded) => {
+  jwt.verify(refreshToken, REFRESH_KEY, async (err, decoded) => {
     if (err) console.log(err);
     if (!decoded) return res.status(403).json({ success: false, message: "유효하지 않은 토큰입니다. "});
 
     return res.status(201).json({
       success: true,
-      accessToken: jwt.sign({ userid: decoded.user_id.toString() }, SECRET_KEY, {
+      accessToken: jwt.sign({ userid: decoded.user_id.toString() }, ACCESS_KEY, {
         expiresIn: '10m',
       }),
     })
