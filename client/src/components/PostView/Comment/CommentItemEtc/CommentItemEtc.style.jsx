462c14d436f8d8d9991609ba89c0202a940048d2
Park Na Yeon <within197@naver.com>
2022-06-30 10:04:25 +0900

    [FEAT] Add component about comment item

---
+++ client/src/components/PostView/Comment/CommentItemEtc/CommentItemEtc.style.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import CommentInput from '../CommentInput/CommentInput';
+import heartFill from '../../../../asset/icon/icon-heart-fill.svg';
+import heartLine from '../../../../asset/icon/icon-heart-line.svg';
+
+export default function CommentItemEtcPresenter({
+  createAt,
+  handleClickShow,
+  likeCount,
+  groupId,
+  postId,
+  parentId,
+  isTargetVisible,
+}) {
+  return (
+    <CommentEtcContainer>
+      <CommentEtc>
+        <CommentDate>{createAt}</CommentDate>
+        <CommentReBtn onClick={handleClickShow}>답글 달기</CommentReBtn>
+        <CommentLike>{likeCount}</CommentLike>
+      </CommentEtc>
+      {isTargetVisible && (
+        <CommentInput
+          groupId={groupId}
+          postId={postId}
+          parentId={parentId}
+          handleClickShow={handleClickShow}
+          isTargetVisible={isTargetVisible}
+        />
+      )}
+    </CommentEtcContainer>
+  );
+}
+
+const CommentEtcContainer = styled.div``;
+
+const CommentEtc = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CommentDate = styled.span`
+  margin-right: 12px;
+  font-size: 14px;
+`;
+
+const CommentReBtn = styled.button`
+  margin-right: 12px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const CommentLike = styled.button`
+  display: flex;
+  align-items: center;
+  &::before {
+    content: '';
+    width: 20px;
+    height: 20px;
+    display: block;
+    background: url(${(props) => (props.like ? `${heartFill}` : `${heartLine}`)});
+    background-position: center;
+    background-size: contain;
+    margin-right: 4px;
+  }
+`;
