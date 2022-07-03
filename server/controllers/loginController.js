e3499c0fdea3b7152cae168d23e04ad78668c760
{hc456123@gmail.com} <hc456123@gmail.com>
2022-07-04 00:36:03 +0900

    [REFACTOR] Update Login Controller
    로그인 컨트롤러 try catch로 서버 문제있을 시 추가

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,29 +4,31 @@ import { createAccessToken, createRefreshToken } from "./tokenControllers.js";
 
 export const postLogin = async (req, res) => {
   const {userId, password} = req.body;
-  const user = await User.findOne({ userId });
-  if (!user) {
-    return res.status(401).json({ success: false, message: "아이디가 존재하지 않습니다." });
-  }
-  if (user) {
-    const ok = await bcrypt.compare(password, user.password);
-    if (!ok) return res.status(401).json({ success: false, message: "비밀번호가 일치하지 않습니다." });
-  }
+  try {
+    const user = await User.findOne({ userId });
+    if (!user) return res.status(401).json({ success: false, message: "아이디가 존재하지 않습니다." });
+    
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(401).json({ success: false, message: "비밀번호가 일치하지 않습니다." });
 
-  const accessToken = createAccessToken(user._id);
-  const refreshToekn = createRefreshToken(user._id);
+    const accessToken = createAccessToken(user._id);
+    const refreshToekn = createRefreshToken(user._id);
 
-  return res.status(200).json({
-    id: user.userId,
-    name: user.nickname,
-    email: user.email,
-    social: user.socialOnly,
-    accessToken,
-    refreshToekn,
-    success: true,
-    imageUrl: user.imageUrl ? user.imageUrl : "",
-    message: "",
-  })
+    return res.status(200).json({
+      id: user.userId,
+      name: user.nickname,
+      email: user.email,
+      social: user.socialOnly,
+      accessToken,
+      refreshToekn,
+      success: true,
+      imageUrl: user.imageUrl ? user.imageUrl : "",
+      message: "",
+    })
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("잠시 후에 다시 시도해주세요.");
+  }
 }
 
 export const getLogout = (req, res) => {
