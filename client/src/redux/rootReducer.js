25a37aa002d21669e5351a0e478c7952fbf4664d
Park Na Yeon <within197@naver.com>
2022-07-07 00:46:12 +0900

    [REFACTOR] Delete postCreate and postForm slice

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -4,8 +4,6 @@ import storage from 'redux-persist/lib/storage';
 
 import userSlice from './slice/userSlice';
 import toggleSlice from './slice/toggleSlice';
-import postCreateSlice from './slice/postCreateSlice';
-import postFormCheckSlice from './slice/postFormCheckSlice';
 import commentCreateSlice from './slice/commentCreateSlice';
 import commentSlice from './slice/commentSlice';
 import postSlice from './slice/postSlice';
@@ -27,8 +25,6 @@ const rootReducer = combineReducers({
   user: persistReducer(userPersistConfig, userSlice.reducer),
   toggle: toggleSlice.reducer,
   authToken: tokenSlice.reducer,
-  postCreate: postCreateSlice,
-  postFormCheck: postFormCheckSlice,
   post: postSlice,
   commentCreate: commentCreateSlice,
   comment: commentSlice,
