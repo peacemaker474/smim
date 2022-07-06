83c9b1a11fed4c1e0fadeae95542169995c58b98
Park Na Yeon <within197@naver.com>
2022-07-07 00:45:48 +0900

    [REFACTOR] Change react hook form

--- client/src/components/post/PostBottomBtn/PostBottomBtn.jsx
+++ client/src/components/post/PostBottomBtn/PostBottomBtn.jsx
@@ -1,59 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { resetPostCreate } from '../../../redux/slice/postCreateSlice';
-import {
-  titleCheck,
-  ageCheck,
-  hashtagCheck,
-  contentCheck,
-  resetPostCheck,
-} from '../../../redux/slice/postFormCheckSlice';
 import PostBottomBtnPresenter from './PostBottomBtn.style';
-import { modalToggle } from '../../../redux/slice/toggleSlice';
 
 function PostBottomBtn({ formState }) {
   const navigate = useNavigate();
-  const postData = useSelector((state) => state.postCreate);
-  const dispatch = useDispatch();
-
-  const handleFormCheck = (data) => {
-    if (data.title === '') {
-      console.log('제목을 입력해주세요');
-      dispatch(titleCheck());
-    } else if (data.targetAge === '') {
-      console.log('연령층을 선택해주세요');
-      dispatch(ageCheck());
-    } else if (data.hashtag.length === 0) {
-      console.log('태그를 입력해주세요');
-      dispatch(hashtagCheck());
-    } else if (data.content === '') {
-      console.log('내용을 입력해주세요');
-      dispatch(contentCheck());
-    } else {
-      dispatch(resetPostCheck());
-      dispatch(modalToggle());
-    }
-  };
 
   const handleFormCancle = () => {
-    dispatch(resetPostCreate());
-    dispatch(resetPostCheck());
     navigate(-1);
   };
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    handleFormCheck(postData);
-  };
-
-  return (
-    <PostBottomBtnPresenter
-      formState={formState}
-      handleFormCancle={handleFormCancle}
-      handleFormSubmit={handleFormSubmit}
-    />
-  );
+  return <PostBottomBtnPresenter formState={formState} handleFormCancle={handleFormCancle} />;
 }
 
 export default PostBottomBtn;
--- client/src/components/post/PostForm/PostForm.style.jsx
+++ client/src/components/post/PostForm/PostForm.style.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import PostTitle from '../PostTitle/PostTitle';
 import PostTargetAge from '../PostTargetAge/PostTargetAge';
-// import PostTag from '../PostTag/PostTag';
+import PostTag from '../PostTag/PostTag';
 import PostEditor from '../PostEditor/PostEditor';
+import PostBottomBtn from '../../../components/post/PostBottomBtn/PostBottomBtn';
 
-function PostFormPresenter({ register, handleSubmit, errors, control }) {
+function PostFormPresenter({
+  register,
+  handleSubmit,
+  errors,
+  setValue,
+  watch,
+  pathValue,
+  modalOpen,
+}) {
   return (
     <form
       id='upload'
-      onSubmit={handleSubmit((data) => {
+      onSubmit={handleSubmit((data, e) => {
+        e.preventDefault();
         console.log(data);
+        modalOpen();
       })}
     >
       <PostTitle register={register} errors={errors} />
       <PostTargetAge register={register} errors={errors} />
-      {/* <PostTag register={register} />
-      <PostEditor register={register} control={control} /> */}
-      <PostEditor register={register} errors={errors} control={control} />
-      <button type='submit'>Submit</button>
+      <PostTag register={register} errors={errors} setValue={setValue} watch={watch} />
+      <PostEditor register={register} errors={errors} setValue={setValue} watch={watch} />
+      <PostBottomBtn formState={pathValue} errors={errors} />
     </form>
   );
 }
