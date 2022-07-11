8a521d0988ea3942dd76e9513c668ccd309c3882
Park Na Yeon <within197@naver.com>
2022-07-11 18:05:49 +0900

    [ERROR] Fix data object property name

--- server/controllers/tokenControllers.js
+++ server/controllers/tokenControllers.js
@@ -4,13 +4,13 @@ import User from '../models/User.js';
 const ACCESS_KEY = process.env.ACCESS_KEY;
 const REFRESH_KEY = process.env.REFRESH_KEY;
 
-export const createAccessToken = ( userId ) => {
+export const createAccessToken = (userId) => {
   return jwt.sign({ user_id: userId.toString() }, ACCESS_KEY, {
     expiresIn: '10m',
   });
 };
 
-export const createRefreshToken = ( userId ) => {
+export const createRefreshToken = (userId) => {
   return jwt.sign({ user_id: userId.toString() }, REFRESH_KEY, {
     expiresIn: '30 days',
   });
@@ -38,27 +38,34 @@ export const verifyToken = async (req, res, next) => {
 
 export const verifyAccessToken = (req, res, next) => {
   const { accessToken } = req.body;
-  if (!accessToken) return res.status(401).json({ success: false, message: "토큰이 존재하지 않습니다. 다시 로그인해주세요."});
+  if (!accessToken)
+    return res
+      .status(401)
+      .json({ success: false, message: '토큰이 존재하지 않습니다. 다시 로그인해주세요.' });
 
   jwt.verify(accessToken, ACCESS_KEY, (err, decoded) => {
-    if (err) return res.status(401).json({ success: false, message: "토큰이 만료되었거나 유효하지 않은 토큰입니다."});
+    if (err)
+      return res
+        .status(401)
+        .json({ success: false, message: '토큰이 만료되었거나 유효하지 않은 토큰입니다.' });
     console.log(decoded);
     next();
-  })
-}
+  });
+};
 
 export const reissueAccessToken = (req, res) => {
   const { refreshToken } = req.body;
 
   jwt.verify(refreshToken, REFRESH_KEY, async (err, decoded) => {
     if (err) console.log(err);
-    if (!decoded) return res.status(403).json({ success: false, message: "유효하지 않은 토큰입니다. "});
+    if (!decoded)
+      return res.status(403).json({ success: false, message: '유효하지 않은 토큰입니다. ' });
 
     return res.status(201).json({
       success: true,
-      accessToken: jwt.sign({ userid: decoded.user_id.toString() }, ACCESS_KEY, {
+      accessToken: jwt.sign({ user_id: decoded.user_id.toString() }, ACCESS_KEY, {
         expiresIn: '10m',
       }),
-    })
-  })
-}
+    });
+  });
+};
