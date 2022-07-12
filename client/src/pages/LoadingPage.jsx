8e098622ffe6f3df8792d532c07e28a076997d6f
Park Na Yeon <within197@naver.com>
2022-07-12 17:14:08 +0900

    [STYLE] Update style props

--- client/src/pages/LoadingPage.jsx
+++ client/src/pages/LoadingPage.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 import { HashLoader } from 'react-spinners';
 import styled from 'styled-components';
 
 const LoadingWrapper = styled.section`
-  position: ${({ position }) => `${position}` || 'fixed'};
-  top: ${({ top }) => `${top}` || "50%"};
-  left: ${({ left }) => `${left}` || '50%'};
-  transform: ${({ top, left }) =>  `translate(-${left}, -${top})`|| `translate(-50%, -50%)`};
+  position: ${({ position }) => (position ? `${position}` : 'fixed')};
+  top: ${({ top }) => (top ? `${top}` : '50%')};
+  left: ${({ left }) => (left ? `${left}` : '50%')};
+  transform: ${({ top, left }) =>
+    left && top ? `translate(-${left}, -${top})` : `translate(-50%, -50%)`};
 `;
 
-export default function LoadingPage({position, top, left}) {
+export default function LoadingPage({ position, top, left }) {
   return (
     <LoadingWrapper position={position} top={top} left={left}>
       <HashLoader />
