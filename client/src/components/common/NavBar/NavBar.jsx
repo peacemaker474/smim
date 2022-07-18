19fb0860bc60b0cd77ab5633a60338445ea5a570
{hc456123@gmail.com} <hc456123@gmail.com>
2022-07-18 15:29:58 +0900

    [STYLE] Update Login and Signup Part
    메인 상단바 수정

--- client/src/components/common/NavBar/NavBar.jsx
+++ client/src/components/common/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { loginToggle, menuToggle } from '../../../redux/slice/toggleSlice';
+import { loginToggle, menuToggle, myPageToggle } from '../../../redux/slice/toggleSlice';
 import { getUserLogOut } from '../../../redux/services/UserService';
 import { DELETE_TOKEN } from '../../../redux/auth';
 import NavBarStyle from './NavBar.style';
@@ -10,8 +10,9 @@ import { deleteCookie } from '../../../utils/cookie';
 function NavBar() {
   const menuToggled = useSelector((state) => state.toggle.menuToggled);
   const loginToggled = useSelector((state) => state.toggle.loginToggled);
+  const myPageToggled = useSelector((state) => state.toggle.myPageToggled);
   const { authenticated } = useSelector((state) => state.authToken);
-  const { social } = useSelector((state) => state.user);
+  const { imgUrl } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { pathname, search } = useLocation(null);
@@ -24,6 +25,7 @@ function NavBar() {
     deleteCookie("users");
     dispatch(getUserLogOut());
     dispatch(DELETE_TOKEN());
+    dispatch(myPageToggle());
     navigate('/');
   }, [dispatch, navigate]);
 
@@ -31,16 +33,22 @@ function NavBar() {
     dispatch(menuToggle());
   }, [dispatch]);
 
+  const handleMyPageClick = useCallback((evt) => {
+    dispatch(myPageToggle());
+  })
+
   return (
     <NavBarStyle
       menuToggled={menuToggled}
       loginToggled={loginToggled}
+      myPageToggled={myPageToggled}
       authenticated={authenticated}
-      social={social}
+      imgUrl={imgUrl}
       pathname={search ? `${pathname}${search}` : `${pathname}`}
       onLoginClick={handleLoginClick}
       onLogoutClick={handleLogoutClick}
       onToggleClick={handleToggleClick}
+      onMyPageClick={handleMyPageClick}
     />
   );
 }
