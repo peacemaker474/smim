558f8d7d438ddb4e8c57c2a48ad843ebca699c1c
Park Na Yeon <within197@naver.com>
2022-08-24 10:01:50 +0900

    [CHORE] Update about post List

--- client/src/utils/changedDate.js
+++ client/src/utils/changedDate.js
@@ -1,6 +1,6 @@
 export default function getDate(data) {
-  var _year = data.getFullYear();
-  var _month = data.getMonth() + 1;
-  var _day = data.getDate();
+  const _year = data.getFullYear();
+  const _month = data.getMonth() + 1;
+  const _day = data.getDate();
   return `${_year}년 ${_month}월 ${_day}일`;
 }
