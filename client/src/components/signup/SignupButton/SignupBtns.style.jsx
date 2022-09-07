32e8a6c25c4e61e6937ecdc69b261f2ef6bc1a2d
Park Na Yeon <within197@naver.com>
2022-09-07 18:38:16 +0900

    [STYLE] style btn about signup

--- client/src/components/signup/SignupButton/SignupBtns.style.jsx
+++ client/src/components/signup/SignupButton/SignupBtns.style.jsx
@@ -10,20 +10,7 @@ const BtnBox = styled.div`
 `;
 
 const SignupBtn = styled(ColorBtn)`
-  background-color: ${({theme}) => theme.color.yellow};
-  border: none;
-  width: 80px;
-  height: 70%;
-  border-radius: 10px;
-  font-size: 0.9rem;
-  font-weight: bold;
-
-  @media screen and (max-width: 400px) {
-    font-size: 0.8rem;
-  }
-`;
-
-const CancelBtn = styled(ColorBtn)`
+  background-color: ${({ theme }) => theme.color.yellow};
   width: 80px;
   height: 70%;
   border: none;
@@ -37,11 +24,24 @@ const CancelBtn = styled(ColorBtn)`
   }
 `;
 
-function SignupBtnsStyle ({ onCancelClick }) {
+const CancelBtn = styled(ColorBtn)`
+  border: none;
+  width: 80px;
+  height: 70%;
+  border-radius: 10px;
+  font-size: 0.9rem;
+  font-weight: bold;
+
+  @media screen and (max-width: 400px) {
+    font-size: 0.8rem;
+  }
+`;
+
+function SignupBtnsStyle({ onCancelClick }) {
   return (
     <BtnBox>
+      <SignupBtn type='submit'> 회원가입 </SignupBtn>
       <CancelBtn onClick={onCancelClick}> 취소 </CancelBtn>
-      <SignupBtn type="submit"> 회원가입 </SignupBtn>
     </BtnBox>
   );
 }
