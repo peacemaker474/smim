bf74f586bc95a24115709631b39c4562e1c85fc0
Park Na Yeon <within197@naver.com>
2022-09-13 09:46:47 +0900

    [STYLE] style about navigator

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,11 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import AppRoute from './routes/AppRoute';
 import LoginSection from './components/login/LoginSection/LoginSection';
 import Auth from './components/common/Auth/Auth';
-import NavBar from './components/common/NavBar/NavBar';
 
 function App() {
   const { loginToggled } = useSelector((state) => state.toggle);
   return (
     <>
-      <NavBar />
       <Auth />
       {loginToggled && <LoginSection />}
       <AppRoute />
--- client/src/routes/PrivateRoute.jsx
+++ client/src/routes/PrivateRoute.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import NavBar from '../components/common/NavBar/NavBar';
 
 function PrivateRoute() {
   const { authenticated } = useSelector((state) => state.authToken);
-  return <>{authenticated ? <Outlet /> : <Navigate to='/' />}</>;
+  return (
+    <>
+      <NavBar />
+      {authenticated ? <Outlet /> : <Navigate to='/' />}
+    </>
+  );
 }
 
 export default PrivateRoute;
