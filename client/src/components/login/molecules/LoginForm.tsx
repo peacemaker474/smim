f75789a6edb20238ca34f151d8636431b719c4d9
{hc456123@gmail.com} <hc456123@gmail.com>
2022-10-21 15:08:03 +0900

    [FEAT] Update Login Form
    로그인 영역 수정

--- client/src/components/login/molecules/LoginForm.tsx
+++ client/src/components/login/molecules/LoginForm.tsx
@@ -3,10 +3,16 @@ import { shallowEqual } from 'react-redux';
 import styled from 'styled-components';
 import { useAppSelector } from '../../../redux/hooks';
 import Label from '../../common/atoms/Label';
+import ValidSpan from '../../common/atoms/ValidSpan';
 import Input from '../atoms/Input';
 
+interface FormValue {
+  userId: string;
+  password: string;
+}
+
 function LoginForm () {
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm<FormValue>({
     mode: 'onBlur',
     reValidateMode: 'onBlur',
   });
@@ -25,14 +31,36 @@ function LoginForm () {
         <Input
           register={register}
           name='userId'
-          required='아이디를 입력하세요'
-          pattern={{
-            value: /^[a-zA-Z0-9]{4,12}$/,
-            message: "4~12자리의 영문, 숫자만 가능합니다."
+          rules={{
+            required: '아이디를 입력하세요',
+            pattern : {
+              value: /^[a-zA-Z0-9]{4,12}$/,
+              message: '4~12자리의 영문, 숫자만 가능합니다.'
+            }
           }}
           type='text'
           placeholder='아이디를 입력하세요.'
         />
+        {errors.userId && <ValidSpan padding='0.6em 0.3em 0 0'> {errors.userId?.message} </ValidSpan>}
+      </InputWrapper>
+      <InputWrapper>
+        <Label> 비밀번호 </Label>
+        <Input
+          register={register}
+          name='password'
+          rules={{
+            required: '비밀번호를 입력하세요.',
+            pattern : {
+              value: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+              message: '8~16자, 최소 하나의 숫자와 특수문자가 필요합니다.'
+            }
+          }}
+          type='password'
+          placeholder='비밀번호를 입력하세요.'
+          autoComplete='off'
+        />
+        {errors.password && <ValidSpan padding='0.6em 0.3em 0 0'> {errors.password?.message} </ValidSpan>}
+        {Object.keys(errors).length === 0 && message && <ValidSpan padding='0.6em 0.3em 0 0'> {message} </ValidSpan>}
       </InputWrapper>
     </FormBox>
   );
