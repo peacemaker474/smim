a7c737294db8b609d9df22c57aff699f1a4380b8
Park Na Yeon <within197@naver.com>
2022-11-07 14:59:10 +0900

    [FEAT] Add Modal and Form Button

--- client/src/components/postCreate/molecules/CreateForm.tsx
+++ client/src/components/postCreate/molecules/CreateForm.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import Title from '../atoms/Title';
 import TargetAge from '../atoms/TargetAge';
 import TagInput from '../atoms/TagInput';
 import Editor from '../atoms/Editor';
+import Buttons from '../atoms/Buttons';
+import Modal from '../../common/molecules/Modal';
 
 interface ParaProps {
   p: string;
@@ -27,6 +30,11 @@ function CreateForm() {
   } = useForm<FormValue>({
     mode: 'onBlur',
   });
+  const { pathname } = useLocation();
+  const pathArr = pathname.split('/');
+  const pathValue = pathArr[2];
+
+  const postUploadToggled = false;
 
   useEffect(() => {
     setValue('title', '');
@@ -35,27 +43,45 @@ function CreateForm() {
     setValue('tagArray', []);
   }, [setValue]);
 
+  const uploadActionFunc = () => {
+    // setView(false);
+    // uploadPost(accessToken);
+    // dispatch(postUploadToggle());
+  };
+
+  const uploadCancleFunc = () => {
+    // dispatch(postUploadToggle());
+  };
+
   return (
-    <form id="upload" method="POST">
-      <Title register={register} errors={errors.title} />
-      <TargetAge register={register} errors={errors.age} />
-      <TagInput
-        register={register}
-        setValue={setValue}
-        clearErrors={clearErrors}
-        watch={watch}
-        // setError={setError}
-        // errors={errors.tagArray}
-      />
-      <Editor
-        register={register}
-        setValue={setValue}
-        clearErrors={clearErrors}
-        watch={watch}
-        // setError={setError}
-        // errors={errors.para}
-      />
-    </form>
+    <>
+      {postUploadToggled && (
+        <Modal actionFunc={uploadActionFunc} cancelFunc={uploadCancleFunc}>
+          {pathValue === 'create' ? '게시물을 등록하겠습니까?' : ' 게시물을 수정하겠습니까?'}
+        </Modal>
+      )}
+      <form id="upload" method="POST">
+        <Title register={register} errors={errors.title} />
+        <TargetAge register={register} errors={errors.age} />
+        <TagInput
+          register={register}
+          setValue={setValue}
+          clearErrors={clearErrors}
+          watch={watch}
+          // setError={setError}
+          // errors={errors.tagArray}
+        />
+        <Editor
+          register={register}
+          setValue={setValue}
+          clearErrors={clearErrors}
+          watch={watch}
+          // setError={setError}
+          // errors={errors.para}
+        />
+        <Buttons />
+      </form>
+    </>
   );
 }
 export default CreateForm;
