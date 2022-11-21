97e3459e8e07cda6cdb25edfb2aaabd37e92132a
Park Na Yeon <within197@naver.com>
2022-11-21 14:02:26 +0900

    [CHORE] Update EtcSpan component

--- client/src/components/common/atoms/EtcSpan.tsx
+++ client/src/components/common/atoms/EtcSpan.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
-// import heartFill from '../../../../asset/icon/icon-heart-fill.svg';
-// import heartLine from '../../../../asset/icon/icon-heart-line.svg';
-// import bookrmarkFill from '../../../../asset/icon/icon-bookrmark-fill.svg';
-// import bookrmarkLine from '../../../../asset/icon/icon-bookrmark-line.svg';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import { getPostLike, getPostUnlike, getBookmark, getUnbookmark } from '../../../networks/post/http';
+import { loginToggle } from '../../../redux/slice/toggleSlice';
 
 interface EtcSpanProps {
-  clickHandler: () => void;
+  // clickHandler: () => void;
   clickState: boolean;
   value: number;
   type: string;
@@ -14,14 +15,53 @@ type ObjType = {
   [index: string]: Array<string>;
 };
 
-function EtcSpan({ type, clickHandler, clickState, value }: EtcSpanProps) {
+function EtcSpan({ type, clickState, value }: EtcSpanProps) {
+  const [isLikeChecked, setIsLikeChecked] = useState(clickState);
+  const [likeValue, setLikeValue] = useState(value);
+  const { accessToken } = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+  const { id } = useParams();
   const etcType: ObjType = {
-    like: ['../../../../asset/icon/icon-heart-fill.svg', '../../../../asset/icon/icon-heart-line.svg'],
-    bookmark: ['bookmarkFill', 'bookmarkLine'],
+    like: [
+      'http://localhost:3000/asset/icons/icon-heart-fill.svg',
+      'http://localhost:3000/asset/icons/icon-heart-line.svg',
+    ],
+    bookmark: [
+      'http://localhost:3000/asset/icons/icon-bookmark-fill.svg',
+      'http://localhost:3000/asset/icons/icon-bookmark-line.svg',
+    ],
   };
+
+  const clickHandler = async () => {
+    if (!accessToken) {
+      dispatch(loginToggle());
+      return;
+    }
+
+    try {
+      if (isLikeChecked && type === 'like') {
+        await getPostUnlike(id, accessToken);
+        setIsLikeChecked(false);
+        setLikeValue((prev) => prev - 1);
+      } else if (type === 'like') {
+        await getPostLike(id, accessToken);
+        setIsLikeChecked(true);
+        setLikeValue((prev) => prev + 1);
+      } else if (isLikeChecked && type === 'bookmark') {
+        await getUnbookmark(id, accessToken);
+        setIsLikeChecked(false);
+      } else {
+        await getBookmark(id, accessToken);
+        setIsLikeChecked(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <PostLikeSpan onClick={clickHandler} checked={clickState} etcType={etcType[type]}>
-      {value}
+    <PostLikeSpan onClick={clickHandler} checked={isLikeChecked} etcType={etcType[type]}>
+      {likeValue === -1 ? '' : likeValue}
     </PostLikeSpan>
   );
 }
