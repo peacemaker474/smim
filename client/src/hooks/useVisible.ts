a876679aa625a301dc63f81c8858ca0f5f278ffa
Park Na Yeon <within197@naver.com>
2022-11-23 13:56:23 +0900

    [FEAT] Add CmntItem component

--- client/src/hooks/useVisible.ts
+++ client/src/hooks/useVisible.ts
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
-function useVisible (state: boolean) {
+function useVisible(state: boolean) {
   const [isTargetVisible, setIsTargetVisible] = useState(state);
 
-  const handleTargetShow = () => {
-    setIsTargetVisible(prev => !prev);
+  const handleTargetShow = (state: boolean) => {
+    setIsTargetVisible(!state);
   };
 
-  return [isTargetVisible, handleTargetShow];
+  return [isTargetVisible, handleTargetShow] as const;
 }
 
 export default useVisible;
--- client/src/redux/rootReducer.ts
+++ client/src/redux/rootReducer.ts
@@ -4,6 +4,7 @@ import toggleSlice from './slice/toggleSlice';
 import userSlice from './slice/userSlice';
 import searchKeywordSlice from './slice/searchKeywordSlice';
 import searchFilterSlice from './slice/searchFilterSlice';
+import commentSlice from './slice/commentSlice';
 
 const rootReducer = combineReducers({
   toggle: toggleSlice.reducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   auth: authSlice.reducer,
   searchKeyword: searchKeywordSlice.reducer,
   searchFilter: searchFilterSlice.reducer,
+  comment: commentSlice.reducer,
 });
 
 export default rootReducer;
