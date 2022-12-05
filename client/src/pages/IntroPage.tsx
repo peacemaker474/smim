4a9de8b738eef8a92e6c910af3b369c2bfe0caa9
{hc456123@gmail.com} <hc456123@gmail.com>
2022-12-05 19:10:07 +0900

    [REFACTOR] Update IntroPage
    소개 페이지 수정

--- client/src/pages/IntroPage.tsx
+++ client/src/pages/IntroPage.tsx
@@ -2,9 +2,6 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../components/common/atoms/Button';
-import Banner1 from '../asset/img1.png';
-import Banner2 from '../asset/img2.png';
-import Banner3 from '../asset/img3.png';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 const Wrapper = styled.div`
@@ -24,26 +21,44 @@ const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  @media screen and (max-width: 928px) {
-    flex-direction: column;
-  }
-`;
+  gap: 5rem;
 
-const Content2 = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const IntroImg = styled.div`
   & > img {
     width: 35em;
   }
-  & > .last-img {
+
+  & > img:last-child {
     width: 50em;
   }
+  
+  &:last-child {
+    flex-direction: column;
+  }
+
+  &:nth-child(2) {
+    flex-direction: row-reverse;
+
+    @media screen and (max-width: 928px) {
+      flex-direction: column;
+    }
+  }
+
+  @media screen and (max-width: 928px) {
+    flex-direction: column;
+    gap: 1rem;
+    & > img {
+      margin-bottom: 2rem;
+    }
+  }
+
+  @media (min-width: 928px) and (max-width: 1040px) {
+    & > img {
+      margin-right: 4rem;
+    }
+  }
+`;
+
+const IntroImg = styled.img`
   @media screen and (max-width: 740px) {
     flex-direction: column;
     margin-right: 0;
@@ -67,33 +82,6 @@ const IntroImg = styled.div`
     & > img {
       width: 30em;
     }
-  }
-`;
-
-const IntroImg1 = styled(IntroImg)`
-  margin-right: 5rem;
-  @media screen and (max-width: 928px) {
-    margin-right: 0;
-    margin-left: 0;
-    & > img {
-      margin-bottom: 2rem;
-    }
-  }
-  @media (min-width: 928px) and (max-width: 1040px) {
-    margin-right: 4rem;
-  }
-`;
-const IntroImg2 = styled(IntroImg)`
-  margin-left: 9rem;
-  @media screen and (max-width: 928px) {
-    margin-right: 0;
-    margin-left: 0;
-    & > img {
-      margin-top: 2rem;
-    }
-  }
-  @media (min-width: 928px) and (max-width: 1040px) {
-    margin-left: 5rem;
   }
 `;
 
@@ -137,6 +125,30 @@ const SubText = styled.p`
 `;
 
 function IntroPage () {
+  const introData = [
+    {
+      id: 1,
+      url: '/images/img2.png',
+      alt: 'Slider이미지2',
+      title: '세대간 궁금했던 이야기를 질문해보세요.',
+      subText: `받는 나이대를 정해 질문을 올릴 수 있어요.`,
+      subText2: `평소 다른 세대에게 궁금했던 점을 태그를 이용해 질문해보세요.`
+    },
+    {
+      id: 2,
+      url: '/images/img3.png',
+      alt: 'Slider이미지3',
+      title: '답변하고 싶은 질문있나요?',
+      subText: `우리 세대에게 온 질문들에 답변할 수 있어요.`,
+      subText2: `댓글 기능으로 답변을 남겨주세요.`,
+    },
+    {
+      id: 3,
+      url: '/images/img1.png',
+      alt: 'Slider이미지1',
+      title: '세대간 소통창구 스며들다, 바로 시작해보세요!',
+    },
+  ]
   const [store, setStore] = useLocalStorage('entry', false);
   const navigate = useNavigate();
 
@@ -155,42 +167,26 @@ function IntroPage () {
 
   return (
     <Wrapper>
-      <Content>
-          <IntroImg1>
-            <img src={Banner2} alt='Slide이미지1' />
-          </IntroImg1>
+      {introData.map(item => (
+        <Content key={item.id}>
+          <IntroImg src={item.url} alt={item.alt} />
           <IntroDesc>
-            <Title>세대간 궁금했던 이야기를 질문해보세요</Title>
-            <SubText>
-              받는 나이대를 정해 질문을 올릴 수 있어요
-              <br />
-              평소 다른 세대에게 궁금했던 점을 태그를 이용해 질문해보세요
-            </SubText>
+            <Title> {item.title} </Title>
+            {item.subText && 
+              <SubText>
+                {item.subText}
+                <br /> 
+                {item.subText2}
+              </SubText>
+            }
+            {item.id === 3 && 
+              <Button width='15rem' height='4rem' border='none' onClick={handleStartClick}>
+                시작하기
+              </Button>
+            }
           </IntroDesc>
         </Content>
-        <Content>
-          <IntroDesc>
-            <Title>답변하고 싶은 질문들이 있나요?</Title>
-            <SubText>
-              우리 세대에게 온 질문들에 답변할 수 있어요 <br />
-              댓글 기능으로 답변을 남겨주세요
-            </SubText>
-          </IntroDesc>
-          <IntroImg2>
-            <img src={Banner3} alt='Slide이미지3' />
-          </IntroImg2>
-        </Content>
-        <Content2>
-          <IntroImg>
-            <img className='last-img' src={Banner1} alt='Slide이미지' />
-          </IntroImg>
-          <IntroDesc>
-            <Title>세대간 소통창구 스며들다, 바로 시작해보세요!</Title>
-            <Button width='15rem' height='4rem' border='none' onClick={handleStartClick}>
-              시작하기
-            </Button>
-          </IntroDesc>
-        </Content2>
+      ))}
     </Wrapper>
   );
 }
